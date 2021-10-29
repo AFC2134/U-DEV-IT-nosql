ca7a66a358286f79fd031521171e1680f8e78a80
afc2134 <85919290+AFC2134@users.noreply.github.com>
2021-10-29 01:10:46 -0500

    put route update candidates party_id

--- server.js
+++ server.js
@@ -131,7 +131,7 @@ app.get('/api/parties', (req, res) => {
     });
   });
 
-  
+
   app.get('/api/party/:id', (req, res) => {
     const sql = `SELECT * FROM parties WHERE id = ?`;
     const params = [req.params.id];
@@ -163,6 +163,29 @@ app.delete('/api/party/:id', (req, res) => {
           message: 'deleted',
           changes: result.affectedRows,
           id: req.params.id
+        });
+      }
+    });
+  });
+
+// Update a candidate's party
+app.put('/api/candidate/:id', (req, res) => {
+    const sql = `UPDATE candidates SET party_id = ? 
+                 WHERE id = ?`;
+    const params = [req.body.party_id, req.params.id];
+    db.query(sql, params, (err, result) => {
+      if (err) {
+        res.status(400).json({ error: err.message });
+        // check if a record was found
+      } else if (!result.affectedRows) {
+        res.json({
+          message: 'Candidate not found'
+        });
+      } else {
+        res.json({
+          message: 'success',
+          data: req.body,
+          changes: result.affectedRows
         });
       }
     });
