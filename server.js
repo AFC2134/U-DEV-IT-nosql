cce5f4844d3e9a544c7be4b23d3de6a956994752
afc2134 <85919290+AFC2134@users.noreply.github.com>
2021-10-29 02:07:35 -0500

    streamlinded server.js file db.connect added

--- server.js
+++ server.js
@@ -1,32 +1,15 @@
 const db = require('./db/connection');
 const express = require('express');
-const inputCheck = require('./utils/inputCheck');
 const PORT = process.env.PORT || 3001;
 const app = express();
 const apiRoutes = require('./routes/apiRoutes');
-
-
-
-
 
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-
 // Add after Express middleware
 app.use('/api', apiRoutes);
-
-
-
-app.get('/', (req, res) => {
-    res.json({
-        message: 'Hello World'
-    });
-});
-
-
-
 
 
 // Default response for any other request (Not Found)
@@ -34,7 +17,11 @@ app.use((req, res) => {
     res.status(404).end();
 });
 
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
-
+// Start server after DB connection
+db.connect(err => {
+    if (err) throw err;
+    console.log('Database connected.');
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  });
